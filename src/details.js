b7e027f66b74d975cc58a6c06bc5323dd5c622c1
Barkha Bhojak <barkha@Barkhas-Macbook.local>
2018-03-20 20:07:26 -0700

    added daily open hours

--- src/details.js
+++ src/details.js
@@ -29,41 +29,46 @@ function getStars(rating) {
 }
 
 function getInfo(places) {
-	if (places.formatted_address !== undefined)
+	var str = "<div class='wrapper-div'><table class='table table-striped'>";
+	
+	if (places.formatted_address !== undefined) {
 		var address = places.formatted_address;
-	else 
-		var address = "";
-	if (places.international_phone_number !== undefined)
+		str += "<tr><th>Address</th><td>" + address + "</td></tr>";
+	}
+	if (places.international_phone_number !== undefined) {
 		var phoneNum = places.international_phone_number;
-	else 
-		var phoneNum = "";
+		str += "<tr><th>Phone Number</th><td>" + phoneNum + "</td></tr>";
+	}
+
 	if (places.price_level !== undefined) {
 		var pLvl = places.price_level + "  ";
 		var t = parseInt(pLvl);
 		for (var i = 0; i < t; i++) {
 			pLvl += "$";
 		}
+		str += "<tr><th>Price Level</th><td>" + pLvl + "</td></tr>";
 	}
-	else 
-		var pLvl = "";
+
 	if (places.rating !== undefined) {
 		var rating = places.rating + " ";
 		rating += getStars(parseFloat(rating));
+		str += "<tr><th>Rating</th><td>" + rating + "</td></tr>";
 	}
-	else 
-		var rating = "";
-	if (places.url !== undefined)
+
+	if (places.url !== undefined) {
 		var gPage = places.url;
-	else 
-		var gPage = "";
-	if (places.website !== undefined)
+		str += "<tr><th>Google Page</th><td><a target='_blank' href='" + gPage +"'>" + gPage + "</a></td></tr>";
+	}
+
+	if (places.website !== undefined) {
 		var web = places.website;
-	else 
-		var web = "";
+		str += "<tr><th>Website</th><td><a target='_blank' href='" + web +"'>" + web + "</a></td></tr>";
+	}
+	var a = false;
 	if (places.opening_hours !== undefined) {
 		var hours = "";
 		if (places.opening_hours.open_now) {
-			hours += "Open now: ";
+			hours += "<b>Open now</b>: ";
 			var day = moment().day();
 			console.log("day = ", day);
 
@@ -72,24 +77,35 @@ function getInfo(places) {
 			hours += currDay;
 		}
 		else {
-			hours += "Closed";
+			hours += "<b>Closed</b>";
 		}
+    	hours += "<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#dailyHoursModal'>  Daily Open Hours</button>"
+		//hours += "  <a data-toggle='modal' data-target='#dailyHoursModal'> Daily open hours</a>";
+		a = true;
+		str += "<tr><th>Hours</th><td>" + hours + "</td></tr>";
 	}
-	else 
-		var hours = "";
 
-	var html = [
-		"<div class='wrapper-div'><table class='table table-striped'>",
-		"<tr><th>Address</th><td>" + address + "</td></tr>",
-		"<tr><th>Phone Number</th><td>" + phoneNum + "</td></tr>",
-		"<tr><th>Price Level</th><td>" + pLvl + "</td></tr>",
-		"<tr><th>Rating</th><td>" + rating + "</td></tr>",
-		"<tr><th>Google Page</th><td><a target='_blank' href='" + gPage +"'>" + gPage + "</a></td></tr>",
-		"<tr><th>Website</th><td><a target='_blank' href='" + web +"'>" + web + "</a></td></tr>",
-		"<tr><th>Hours</th><td>" + hours + "</td></tr>",
-		"</div></table>"
+	str += "</div></table>";
 
-	].join("\n");
+	if (a) {
+		console.log('hello true');
+		str += "<div class='modal fade' tabindex='-1' role='dialog' id='dailyHoursModal'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Open Hours</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'>";
+		str += "<table class='table'>"
+		var dayW = moment().day();
+		for (var b = 0; b < 7; b++) {
+			var dayList = places.opening_hours.weekday_text[b].split('y:')[0] + "y";
+			var timings = places.opening_hours.weekday_text[b].split('y:')[1];
+			if (b === dayW) {
+				str += "<tr><th>" + dayList + "</th><td>" + timings + "</td></tr>";
+			}
+			else {
+				str += "<tr><td>" + dayList + "</td><td>" + timings + "</td></tr>";
+			}
+		}
 
-	return html;
+		str += "</table></div><div class='modal-footer'><button type='button' class='btn btn-secondary' data dismiss='modal'>Close</button></div></div></div></div>";
+
+	}
+
+	return str;
 }
